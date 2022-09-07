a558052a5f7c48d2befdfc464f02390aff136acd
vighnesh192 <vighnesh192000@gmail.com>
2022-09-08 00:40:45 +0530

    Feat: Created Abstract Repository for Database Calls

---
+++ src/database/entity.repository.ts
@@ -0,0 +1,33 @@
+import { Document, FilterQuery, Model, UpdateQuery } from "mongoose";
+
+export abstract class EntityRepository<T extends Document> {
+    constructor(protected readonly entityModel: Model<T>) { }
+
+    async findOne(
+        entityFilterQuery: FilterQuery<T>,
+        projection: Record<string, unknown>
+    ): Promise<T | null> {
+        // The properties present inside projection would be ignored by findOne
+        return this.entityModel.findOne(entityFilterQuery, { ...projection }).exec();
+    }
+
+    async find(entityFilterQuery: FilterQuery<T>): Promise<T[] | null> {
+        return this.entityModel.find(entityFilterQuery);
+    }
+
+    async create(entityDetails: unknown): Promise<T> {
+        return this.entityModel.create(entityDetails);
+    }
+
+    async findOneAndUpdate(
+        entityFilterQuery: FilterQuery<T>,
+        updatedEntityDetails: UpdateQuery<unknown>
+    ): Promise<T | null> {
+        return this.entityModel.findOneAndUpdate(entityFilterQuery, updatedEntityDetails, { new: true })
+    }
+
+    async deleteMany(entityFilterQuery: FilterQuery<T>): Promise<boolean> {
+        const result = await this.entityModel.deleteMany(entityFilterQuery);
+        return result.deletedCount >= 1;
+    }
+}
--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,26 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { User, UserDocument } from './schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, FilterQuery } from 'mongoose';
+import { Model } from 'mongoose';
+import { EntityRepository } from 'src/database/entity.repository';
 
 @Injectable()
-export class UsersRepository {
-    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) { }
-
-    async findOne(userFilterQuery: FilterQuery<User>): Promise<User> {
-        return this.userModel.findOne(userFilterQuery);
-    }
-
-    async find(userFilterQuery: FilterQuery<User>): Promise<User[]> {
-        return this.userModel.find(userFilterQuery);
-    }
-
-    async create(user: User): Promise<User> {
-        const newUser = new this.userModel(user);
-        return newUser;
-    }
-
-    async findOneAndUpdate(userFilterQuery: FilterQuery<User>, user: Partial<User>): Promise<User> {
-        return this.userModel.findOneAndUpdate(userFilterQuery, user, { new: true });
+export class UsersRepository extends EntityRepository<UserDocument> {
+    constructor(@InjectModel(User.name) userModel: Model<UserDocument>) {
+        super(userModel)
     }
 }
